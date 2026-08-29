f5cd1c9f313b09875258982a0abec669885dbe6e
VitorVT9 <vsilvab57@gmail.com>
2026-08-29 15:48:56 -0300

    feat: cria função para adição de novas tarefas

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import {reactive} from 'vue';
 
   const estado = reactive({
     filtro: 'todas',
+    tarefaTemp: '',
     tarefas: [
       {
         titulo: 'Estudar ES6',
@@ -39,6 +40,16 @@ import {reactive} from 'vue';
         return estado.tarefas;
     }
   }
+
+  const cadastraTarefa = () => {
+    const tarefaNova = {
+      titulo: estado.tarefaTemp,
+      finalizada: false,
+    }
+
+    estado.tarefas.push(tarefaNova);
+    estado.tarefaTemp = '';
+  }
 </script>
 
 <template>
@@ -49,10 +60,10 @@ import {reactive} from 'vue';
         Você possui {{ getTarefasPendentes().length }} tarefas pendentes
       </p>
     </header>
-    <form>
+    <form @submit.prevent="cadastraTarefa">
       <div class="row">
         <div class="col">
-          <input type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+          <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
         </div>
         <div class="col-md-2">
           <button type="submit" class="btn btn-primary">Cadastrar</button>
@@ -68,7 +79,7 @@ import {reactive} from 'vue';
     </form>
     <ul class="list-group mt-4">
       <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
-        <input :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+        <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
         <label :class="{done: tarefa.finalizada}" class="ms-3" :for="tarefa.titulo">
           {{ tarefa.titulo }}
         </label>
