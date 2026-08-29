6aede463844f109b03c377712e5bf2eb03acfa70
VitorVT9 <vsilvab57@gmail.com>
2026-08-29 15:13:24 -0300

    feat: cria formulário base para listagem de tarefas

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,31 @@
 </script>
 
 <template>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p>
+        Você possui 7 tarefas pendentes
+      </p>
+    </header>
+    <form>
+      <div class="row">
+        <div class="col">
+          <input type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+        </div>
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+          <select class="form-control">
+            <option value="todas">Todas as tarefas</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
